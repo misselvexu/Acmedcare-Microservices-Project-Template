13c70fd85303486d0562ddfe3f6e12f840270561
misselvexu <iskp.me@gmail.com>
2019-03-06 18:57:37 +0800

    Add test case;

---
+++ repository/repository-jdbc/src/test/java/com/acmedcare/framework/microservices/template/repository/jdbc/AccountJDBCRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.acmedcare.framework.microservices.template.repository.jdbc;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * AccountJDBCRepositoryTest
+ *
+ * @author <a href="mailto:iskp.me@gmail.com">Elve.Xu</a>
+ * @version ${project.version} - 2019-03-06.
+ */
+public class AccountJDBCRepositoryTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void queryAccount() {}
+}
