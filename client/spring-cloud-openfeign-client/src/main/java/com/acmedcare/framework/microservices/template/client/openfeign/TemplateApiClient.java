24186a6f054e7257f2c48cf79dce9c80e3596e42
misselvexu <iskp.me@gmail.com>
2019-03-13 00:51:01 +0800

    Add common lib versions;

--- client/spring-cloud-openfeign-client/src/main/java/com/acmedcare/framework/microservices/template/client/openfeign/TemplateApiClient.java
+++ client/spring-cloud-openfeign-client/src/main/java/com/acmedcare/framework/microservices/template/client/openfeign/TemplateApiClient.java
@@ -3,6 +3,10 @@ package com.acmedcare.framework.microservices.template.client.openfeign;
 import com.acmedcare.framework.microservices.template.client.openfeign.configuration.DefaultFeginConfiguration;
 import com.acmedcare.framework.microservices.template.common.Constants;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * TemplateApiClient
@@ -13,8 +17,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 @FeignClient(
     name = Constants.PROJECT_SERVICE_NAMESPACE,
     configuration = DefaultFeginConfiguration.class)
-public final class TemplateApiClient {
+public interface TemplateApiClient {
 
+  /**
+   *
+   * @param passport
+   * @return
+   */
+  @GetMapping(value = "/{passport}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  ResponseEntity queryAccount(@PathVariable("passport") String passport);
 
 
 
