110f5fd6276e40e6d30a07b333aa6948bff54cae
misselvexu <iskp.me@gmail.com>
2019-03-12 13:08:23 +0800

    Add mybatis support; Add spring boot & spring cloud distribution module implements;

--- api/src/main/java/com/acmedcare/framework/microservices/template/bean/Account.java
+++ api/src/main/java/com/acmedcare/framework/microservices/template/bean/Account.java
@@ -1,7 +1,7 @@
 package com.acmedcare.framework.microservices.template.bean;
 
 import com.acmedcare.framework.microservices.template.common.Constants;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,7 +18,6 @@ import java.util.Date;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Account implements Serializable {
 
   private static final long serialVersionUID = -6625478147696736439L;
@@ -53,4 +52,24 @@ public class Account implements Serializable {
 
   /** 更新时间 */
   private Date upgradeTime;
+
+  @Builder
+  public Account(
+      long pkid,
+      String passport,
+      String username,
+      String password,
+      String salt,
+      Constants.AccountStatus status,
+      Date createTime,
+      Date upgradeTime) {
+    this.pkid = pkid;
+    this.passport = passport;
+    this.username = username;
+    this.password = password;
+    this.salt = salt;
+    this.status = status;
+    this.createTime = createTime;
+    this.upgradeTime = upgradeTime;
+  }
 }
--- repository/repository-jdbc/src/main/java/com/acmedcare/framework/microservices/template/repository/jdbc/AccountJDBCRepository.java
+++ repository/repository-jdbc/src/main/java/com/acmedcare/framework/microservices/template/repository/jdbc/AccountJDBCRepository.java
@@ -1,6 +1,7 @@
 package com.acmedcare.framework.microservices.template.repository.jdbc;
 
 import com.acmedcare.framework.microservices.template.bean.Account;
+import com.acmedcare.framework.microservices.template.common.Constants;
 import com.acmedcare.framework.microservices.template.common.exception.RepositoryException;
 import com.acmedcare.framework.microservices.template.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,8 +28,21 @@ public class AccountJDBCRepository implements AccountRepository {
   @Override
   public Account queryAccount(String passport) throws RepositoryException {
 
-    // TODO query account
-
-    return null;
+    String sql =
+        "SELECT `pkid`, `passport`, `username`, `password`, `salt` , `status`, `create_time`, `upgrade_time` FROM `acmedcare_account` WHERE `passport` = ? LIMIT 1";
+    return this.jdbcTemplate.queryForObject(
+        sql,
+        (rs, rowNum) ->
+            Account.builder()
+                .pkid(rs.getLong("pkid"))
+                .passport(rs.getString("passport"))
+                .password(rs.getString("password"))
+                .username(rs.getString("username"))
+                .salt(rs.getString("salt"))
+                .status(Constants.AccountStatus.valueOf(rs.getString("status")))
+                .createTime(rs.getDate("create_time"))
+                .upgradeTime(rs.getDate("upgrade_time"))
+                .build(),
+        passport);
   }
 }
